45920dce66f001dc3b75bd8d5b99cd13b7f1d7de
Charles Wrightson <charles@labsite.ca>
2020-09-12 16:08:41 -0600

    Add Chapter 06 exercise 04

--- chapter-06-exercises/exercise-4/exercise.R
+++ chapter-06-exercises/exercise-4/exercise.R
@@ -4,29 +4,44 @@
 # returns whether or not (e.g., a boolean) the length of one argument is greater
 # than or equal to twice the length of the other.
 # Hint: compare the length difference to the length of the smaller string
-twice_as_long <- function(word_1, word_2){
-  
-  if((nchar(word_1) > nchar(word_2)) && (nchar(word_1) != nchar(word_2))){
+
+twice_as_long <- function(word_1, word_2) {
+  if ((nchar(word_1) > nchar(word_2)) &&
+      (nchar(word_1) != nchar(word_2))) {
     large_word <- word_1
     small_word <- word_2
+    print("Is word #1 two times greater than word #2")
+    return(nchar(large_word) >= 2 * nchar(small_word))
+    
   }
   
-  else if((nchar(word_1) < nchar(word_2)) && (nchar(word_1) != nchar(word_2))){
+  else if ((nchar(word_1) < nchar(word_2)) &&
+           (nchar(word_1) != nchar(word_2))) {
     large_word <- word_2
     small_word <- word_1
+    print("Is word #2 two times greater than word #1")
+    return(nchar(large_word) >= 2 * nchar(small_word))
   }
   
-  else if(nchar(word_1) == nchar(word_2)){
+  else if (nchar(word_1) == nchar(word_2)) {
     print("The words are the same length")
     return()
   }
-  return(nchar(large_word) >= 2*nchar(small_word))
 }
 
 # Call your `is_twice_as_long` function by passing it different length strings
 # to confirm that it works. Make sure to check when _either_ argument is twice
 # as long, as well as when neither are!
-twice_as_long("Eleven","Two")
+
+twice_as_long("Twenty Three","Two")
+# [1] "Is word #1 two times greater than word #2"
+# [1] TRUE
+# > twice_as_long("Ele","Two")
+# [1] "The words are the same length"
+# NULL
+# > twice_as_long("Ele","Two00000")
+# [1] "Is word #2 two times greater than word #1"
+# [1] TRUE
 
 
 # Define a function `describe_difference` that takes in two strings. The
@@ -35,7 +50,33 @@ twice_as_long("Eleven","Two")
 #   "Your second string is longer by N characters"
 #   "Your strings are the same length!"
 
+describe_difference <- function(word_1, word_2){
+  
+  if ((nchar(word_1) > nchar(word_2)) &&
+      (nchar(word_1) != nchar(word_2))) {
+    difference <- nchar(word_1) - nchar(word_2)
+    print(paste("Your first string is longer by", difference, "charatcer"))
+ 
+    
+    
+  }
+  
+  else if(nchar(word_1) != nchar(word_2)){
+    difference <- nchar(word_2) - nchar(word_1)
+    print(paste("Your second string is longer by", difference, "character"))
+ 
+    
+  }
+  
+  else if(nchar(word_1) == nchar(word_2)){
+    print("Your strings are the same length")
+
+  }
+
+}
+
 
 # Call your `describe_difference` function by passing it different length strings
 # to confirm that it works. Make sure to check all 3 conditions1
 
+describe_difference("Twenty", "Two")
