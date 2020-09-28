f8d3b098d2a16c034090341301cfc3ae093a127a
Charles Wrightson <charles@labsite.ca>
2020-09-27 23:24:41 -0600

    Add Chapter #7 Exercise 02

--- chapter-07-exercises/exercise-2/exercise.R
+++ chapter-07-exercises/exercise-2/exercise.R
@@ -26,27 +26,36 @@ even <- seq(2,100,by=2)
 
 
 check_even <- all(even%%2 == 0)
-
+check_even
 
 
 
 
 # Create a vector `phone_numbers` that contains the numbers 8, 6, 7, 5, 3, 0, 9
 
+phone_numbers <- c(8,6,7,5,3,0,9)
 
 # Create a vector `prefix` that has the first three elements of `phone_numbers`
 
+indices <- c(1,2,3)
+prefix <- phone_numbers[indices] 
 
 # Create a vector `small` that has the values of `phone_numbers` that are 
 # less than or equal to 5
 
+less_than_or_equal_to_5 <- phone_numbers <= 5
+small <- phone_numbers[less_than_or_equal_to_5]
 
 # Create a vector `large` that has the values of `phone_numbers` that are 
 # strictly greater than 5
 
+greater_than_5 <- phone_numbers > 5
+large <- phone_numbers [greater_than_5]
 
 # Replace the values in `phone_numbers` that are larger than 5 with the number 5
 
+phone_numbers[phone_numbers > 5] <- 5
 
 # Replace every odd-numbered value in `phone_numbers` with the number 0
 
+phone_numbers[phone_numbers %%2 != 0] <- 0 
